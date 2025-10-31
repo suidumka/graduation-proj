6391549039e50b77e945e16ed9ac13b4484c00cd
suidumka <jalilova.s.m@gmail.com>
2025-10-31 14:27:57 -0400

    new driver + pom + config

--- src/test/java/io/loop/runner/CukesRunner.java
+++ src/test/java/io/loop/runner/CukesRunner.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/html-reports/cucumber-report.html",
-                "json:target/json-reports/json-report.json",
+        plugin = {"json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt"
+
         },
         features = {
                 "src/test/resources/features_ui",

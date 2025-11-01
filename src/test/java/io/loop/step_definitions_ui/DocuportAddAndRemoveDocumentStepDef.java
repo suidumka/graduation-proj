4ef474819d2c439ec971246a1e270608f642ba77
TarasKRV <spirit.see@icloud.com>
2025-11-01 10:44:25 -0500

    Changes

--- src/test/java/io/loop/step_definitions_ui/DocuportAddAndRemoveDocumentStepDef.java
+++ src/test/java/io/loop/step_definitions_ui/DocuportAddAndRemoveDocumentStepDef.java
@@ -82,7 +82,8 @@ public class DocuportAddAndRemoveDocumentStepDef {
     public void user_upload_an_document() throws Exception {
         WebElement element = Driver.getDriver().findElement(By.xpath("//input[@type='file']"));
         element.sendKeys("https://github.com/suidumka/graduation-proj/blob/630304a4c5e56e657850fdf56f17b286127d2693/src/test/resources/Text");
-       
+
+
 
         Thread.sleep(2000);
         pages.getHomePage().clientsLabel.click();

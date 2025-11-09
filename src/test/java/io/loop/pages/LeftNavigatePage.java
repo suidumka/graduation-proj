b7048e462d0f73e760dbade31ec502e82256109d
TarasKRV <spirit.see@icloud.com>
2025-11-09 13:46:19 -0600

    Changes

--- src/test/java/io/loop/pages/LeftNavigatePage.java
+++ src/test/java/io/loop/pages/LeftNavigatePage.java
@@ -74,8 +74,8 @@ public class LeftNavigatePage extends Base {
 
             case "invitations" ->{
                 try {
-                    BrowsersUtils.waitForClickable(invitationsButton, DocuportConstants.EXTRA_LARGE).click();
-                   // BrowsersUtils.clickWithJS(invitationsButton);
+                   // BrowsersUtils.waitForClickable(invitationsButton, DocuportConstants.EXTRA_LARGE).click();
+                    BrowsersUtils.clickWithJS(invitationsButton);
 
                 }
                 catch (StaleElementReferenceException se){
